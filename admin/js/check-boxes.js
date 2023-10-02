9d7851a1f27f09ee52c85d7d253f10f0799e97e7
Ayaz Khatri <ayaz.khatri919@gmail.com>
2023-10-02 18:01:06 +0500

    users data fetch operation completed parrtially

---
+++ admin/js/check-boxes.js
@@ -0,0 +1,53 @@
+/*----------------------------------------------------
+    Block and Delete button disable based on checkboxes
+------------------------------------------------------*/
+
+document.querySelectorAll('#myTable input[type="checkbox"]').forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+        var a = document.querySelectorAll('#myTable input[type="checkbox"]:checked').length;
+        if (a > 0) {
+            document.getElementById('deleteSelected').classList.remove('disabled');
+            document.getElementById('blockSelected').classList.remove('disabled');
+        } else {
+            document.getElementById('deleteSelected').classList.add('disabled');
+            document.getElementById('blockSelected').classList.add('disabled');
+        }
+    });
+});
+
+
+/*----------------------------------------------------
+    Check all checkboxes
+------------------------------------------------------*/
+
+document.querySelector('#myTable #checkAll').addEventListener('click', function () {
+    var isChecked = this.checked;
+    var checkboxes = document.querySelectorAll('#myTable tr:has(td) input[type="checkbox"]');
+
+    checkboxes.forEach(function (checkbox) {
+        checkbox.checked = isChecked;
+    });
+});
+
+var checkboxes = document.querySelectorAll('#myTable tr:has(td) input[type="checkbox"]');
+var checkAllCheckbox = document.querySelector('#checkAll');
+
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('click', function () {
+        var isChecked = this.checked;
+        var isHeaderChecked = checkAllCheckbox.checked;
+
+        if (!isChecked && isHeaderChecked) {
+            checkAllCheckbox.checked = isChecked;
+        } else {
+            var allUnchecked = true;
+
+            checkboxes.forEach(function (checkbox) {
+                if (!checkbox.checked) {
+                    allUnchecked = false;
+                }
+            });
+            checkAllCheckbox.checked = allUnchecked;
+        }
+    });
+});
--- admin/users.php
+++ admin/users.php
@@ -17,15 +17,15 @@
 
     <?php include('includes/admin-navbar.php'); ?>
     <?php include('includes/admin-sidebar.php'); ?>
-    <div class="container my-5">
+    <div class="container my-4">
         <div class="row px-2">
             <div class="col-6">
                 <h2 class="text-danger fw-bold">Users</h2>
             </div>
             <div class="col-6 text-end">
                 <div class="btn-group" role="group" aria-label="Basic outlined example">
-                    <a href="add-user.php" type="button" class="btn btn-sm btn-outline-secondary">Add New User</a>
-                    <!-- <a href="#" type="button" class="btn btn-sm btn-outline-danger">Blocked Users</a> -->
+                    <a href="add.php" type="button" class="btn btn-sm btn-outline-secondary">Add New User</a>
+                    <a href="#" type="button" class="btn btn-sm btn-outline-danger">Blocked Users</a>
                 </div>
             </div>
         </div>
@@ -55,7 +55,7 @@
 
                 <?php 
                 $paramList = [];
-                $sql = "SELECT * FROM users WHERE user_role = 'u' AND user_status = 1";
+                $sql = "SELECT * FROM users WHERE user_role = 'u' AND user_status = 1 ORDER BY user_id DESC";
                 $result = $obj->executeSQL($sql, $paramList , true);
                 foreach($result as $res) { 
                     ?>
@@ -69,17 +69,15 @@
                     <td><?php echo date('Y-M-d | h:i:s A', strtotime($res["created_at"])) ?></td>
                     <td><?php echo date('Y-M-d | h:i:s A', strtotime($res["updated_at"])) ?></td>
                     <td>
-                        <a href="#" class="btn btn-primary btn-sm" id="btnEdit">
+                        <a href="edit.php?id=<?php echo $res['user_id'] ?>" class="btn btn-primary btn-sm" id="btnEdit">
                         <i class="fa fa-edit" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit"></i>
                         </a>
                         |
-                        <a href="abc.html" class="btn btn-warning btn-sm" id="btnTrash" data-bs-toggle="modal"
-                        data-bs-target="#modal">
-                        <i class="fa fa-trash-alt" data-bs-toggle="tooltip" data-bs-placement="top" title="Trash"></i>
+                        <a href="#" class="block btn btn-warning btn-sm" data-id=<?php echo $res['user_id'] ?>>
+                        <i class="fa fa-trash-alt" data-bs-toggle="tooltip" data-bs-placement="top" title="Block"></i>
                         </a>
                         |
-                        <a href="#" class="btn btn-danger btn-sm" id="btnDelete" data-bs-toggle="modal"
-                        data-bs-target="#modal">
+                        <a href="#" class="delete btn btn-danger btn-sm" data-id=<?php echo $res['user_id'] ?>>
                         <i class="fa fa-times-circle" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"></i>
                         </a>
                     </td>
@@ -90,34 +88,34 @@
                 </table>
 
                 <div class="tableOptions">
-                <div class="btn-group btn-group-sm">
-                    <a type="button" class="btn btn-success" href="users.php" id="refreshButton">Refresh</a>
-                </div>
-                <div class="btn-group btn-group-sm">
-                    <a href="#" type="button" class="btn btn-warning disabled" id="trashSelectedButton"
-                    data-bs-toggle="modal" data-bs-target="#modal">Trash</a>
-                    <a type="button" class="btn btn-warning dropdown-toggle dropdown-toggle-split"
-                    data-bs-toggle="dropdown">
-                    <span class="visually-hidden">Toggle Dropdown</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                    <li><a class="dropdown-item small" href="#" id="trashAll" data-bs-toggle="modal"
-                        data-bs-target="#modal">Trash All</a></li>
-                    </ul>
-                </div>
-                <div class="btn-group btn-group-sm">
-                    <a type="button" href="#" class="btn btn btn-danger disabled" id="deleteSelectedButton"
-                    data-bs-toggle="modal" data-bs-target="#modal">Delete</a>
-                    <a type="button" class="btn btn btn-danger dropdown-toggle dropdown-toggle-split"
-                    data-bs-toggle="dropdown">
-                    <span class="visually-hidden">Toggle Dropdown</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                    <li><a class="dropdown-item small" href="#" id="deleteAll" data-bs-toggle="modal"
-                        data-bs-target="#modal">Delete All</a></li>
-                    </ul>
-                </div>
-                <hr>
+                    <div class="btn-group btn-group-sm">
+                        <a type="button" class="btn btn-success" href="<?php echo $_SERVER['PHP_SELF'] ?>" id="refreshButton">Refresh</a>
+                    </div>
+                    <div class="btn-group btn-group-sm">
+                        <a href="#" type="button" class="btn btn-warning disabled" id="blockSelected"
+                        data-bs-toggle="modal" data-bs-target="#blockSelectedModal">Block</a>
+                        <a type="button" class="btn btn-warning dropdown-toggle dropdown-toggle-split"
+                        data-bs-toggle="dropdown">
+                        <span class="visually-hidden">Toggle Dropdown</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                        <li><a class="dropdown-item small" href="#" id="blockAll" data-bs-toggle="modal"
+                            data-bs-target="#blockAllModal">Block All</a></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group btn-group-sm">
+                        <a type="button" href="#" class="btn btn btn-danger disabled" id="deleteSelected"
+                        data-bs-toggle="modal" data-bs-target="#deleteSelectedModal">Delete</a>
+                        <a type="button" class="btn btn btn-danger dropdown-toggle dropdown-toggle-split"
+                        data-bs-toggle="dropdown">
+                        <span class="visually-hidden">Toggle Dropdown</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                        <li><a class="dropdown-item small" href="#" id="deleteAll" data-bs-toggle="modal"
+                            data-bs-target="#deleteAllModal">Delete All</a></li>
+                        </ul>
+                    </div>
+                    <hr>
                 </div>
             </div>
             </div>
@@ -125,8 +123,8 @@
         </div>
     </div>
 
-    <!-- The Modal -->
-    <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <!-- The Delete Modal -->
+    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
@@ -135,8 +133,8 @@
             </div>
             <div class="modal-body text-center">
             <form action="#">
-                <p id="modalMessage">Are you sure you want to delete your account?</p>
-                <button type="submit" class="btn btn-danger w-25">Yes</button>
+                <p id="modalMessage">Are you sure you want to permanantly delete this user?</p>
+                <a href="#" id="deleteLink" class="btn btn-danger w-25">Yes</a>
                 <button type="button" class="btn btn-secondary w-25" data-bs-dismiss="modal">No</button>
             </form>
             </div>
@@ -145,7 +143,111 @@
         </div>
     </div>
 
+    <!-- The Delete Selected Modal -->
+    <div class="modal fade" id="deleteSelectedModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Confirmation Message!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                <form id="deleteSelectedForm" method="POST" action="delete-selected.php">
+                    <p id="modalMessage">Are you sure you want to permanantly delete selected user[s]?</p>
+                    <!-- Hidden input fields to store selected IDs -->
+                    <input type="hidden" name="selectedIds" id="selectedDeleteIds" value="">
+                    <button type="submit" class="btn btn-danger w-25">Yes</button>
+                    <button type="button" class="btn btn-secondary w-25" data-bs-dismiss="modal">No</button>
+                </form>
+            </div>
+            <div class="modal-footer"></div>
+        </div>
+        </div>
+    </div>
 
+    <!-- The Delete All Modal -->
+    <div class="modal fade" id="deleteAllModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Confirmation Message!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+            <form action="#">
+                <p id="modalMessage">Are you sure you want to permanantly delete all users?</p>
+                <a href="delete-all.php" class="btn btn-danger w-25">Yes</a>
+                <button type="button" class="btn btn-secondary w-25" data-bs-dismiss="modal">No</button>
+            </form>
+            </div>
+            <div class="modal-footer"></div>
+        </div>
+        </div>
+    </div>
+
+    <!-- The Block Modal -->
+    <div class="modal fade" id="blockModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Confirmation Message!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+            <form action="#">
+                <p id="modalMessage">Are you sure you want to block this user?</p>
+                <a href="#" id="blockLink" class="btn btn-danger w-25">Yes</a>
+                <button type="button" class="btn btn-secondary w-25" data-bs-dismiss="modal">No</button>
+            </form>
+            </div>
+            <div class="modal-footer"></div>
+        </div>
+        </div>
+    </div>
+
+    <!-- The Block Selected Modal -->
+    <div class="modal fade" id="blockSelectedModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Confirmation Message!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                <form id="blockSelectedForm" method="POST" action="block-selected.php">
+                    <p id="modalMessage">Are you sure you want to block selected user[s]?</p>
+                    <!-- Hidden input fields to store selected IDs -->
+                    <input type="hidden" name="selectedIds" id="selectedBlockIds" value="">
+                    <button type="submit" class="btn btn-danger w-25">Yes</button>
+                    <button type="button" class="btn btn-secondary w-25" data-bs-dismiss="modal">No</button>
+                </form>
+            </div>
+            <div class="modal-footer"></div>
+        </div>
+        </div>
+    </div>
+
+    <!-- The Block Selected Modal -->
+    <div class="modal fade" id="blockAllModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Confirmation Message!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+            <form action="#">
+                <p id="modalMessage">Are you sure you want to block all users?</p>
+                <a href="block-all.php" class="btn btn-danger w-25">Yes</a>
+                <button type="button" class="btn btn-secondary w-25" data-bs-dismiss="modal">No</button>
+            </form>
+            </div>
+            <div class="modal-footer"></div>
+        </div>
+        </div>
+    </div>
+
+<script src="js/check-boxes.js"></script>
 <script>
 
     /*--------------------------
@@ -159,11 +261,8 @@
           orderable: false, // set orderable false for selected columns
         },
       ],
-      order: [[2, 'asc']],
+      order: [[2, 'desc']],
     });
-
-
-
 
 
     function onImgError(source)
@@ -174,78 +273,75 @@
         return true;
     }
 
-     /*----------------------------------------------------
-        Trash and Delete button disable based on checkboxes
-      ------------------------------------------------------*/
+    document.addEventListener("DOMContentLoaded", function() {
+        var deleteLinks = document.querySelectorAll(".delete");
+        var blockLinks = document.querySelectorAll(".block");
 
-    document.querySelectorAll('#myTable input[type="checkbox"]').forEach(function (checkbox) {
-      checkbox.addEventListener('change', function () {
-        var a = document.querySelectorAll('#myTable input[type="checkbox"]:checked').length;
-        if (a > 0) {
-          document.getElementById('deleteSelectedButton').classList.remove('disabled');
-          document.getElementById('trashSelectedButton').classList.remove('disabled');
-        } else {
-          document.getElementById('deleteSelectedButton').classList.add('disabled');
-          document.getElementById('trashSelectedButton').classList.add('disabled');
-        }
-      });
-    });
+        deleteLinks.forEach(function(link) {
+            link.addEventListener("click", function(event) {
+                event.preventDefault();
+                var id = this.getAttribute("data-id");
+                var deleteLink = document.getElementById("deleteLink");
 
+                // Set the "Delete" link's href dynamically
+                deleteLink.href = 'delete.php?id=' + id;
 
-    /*----------------------------------------------------
-        Check all checkboxes
-    ------------------------------------------------------*/
-
-      document.querySelector('#myTable #checkAll').addEventListener('click', function () {
-      var isChecked = this.checked;
-      var checkboxes = document.querySelectorAll('#myTable tr:has(td) input[type="checkbox"]');
-      
-      checkboxes.forEach(function (checkbox) {
-          checkbox.checked = isChecked;
-      });
-    });
-
-    var checkboxes = document.querySelectorAll('#myTable tr:has(td) input[type="checkbox"]');
-    var checkAllCheckbox = document.querySelector('#checkAll');
-
-    checkboxes.forEach(function (checkbox) {
-        checkbox.addEventListener('click', function () {
-            var isChecked = this.checked;
-            var isHeaderChecked = checkAllCheckbox.checked;
-            
-            if (!isChecked && isHeaderChecked) {
-                checkAllCheckbox.checked = isChecked;
-            } else {
-                var allUnchecked = true;
-                
-                checkboxes.forEach(function (checkbox) {
-                    if (!checkbox.checked) {
-                        allUnchecked = false;
-                    }
-                });
-                
-                console.log(allUnchecked);
-                checkAllCheckbox.checked = allUnchecked;
-            }
+                // Show the modal
+                var deleteModal = document.getElementById("deleteModal");
+                var modal = new bootstrap.Modal(deleteModal);
+                modal.show();
+            });
         });
+
+        blockLinks.forEach(function(link) {
+            link.addEventListener("click", function(event) {
+                event.preventDefault();
+                var id = this.getAttribute("data-id");
+                var blockLink = document.getElementById("blockLink");
+
+                // Set the "Block" link's href dynamically
+                blockLink.href = 'block.php?id=' + id;
+
+                // Show the modal
+                var blockModal = document.getElementById("blockModal");
+                var modal = new bootstrap.Modal(blockModal);
+                modal.show();
+            });
+        });
+
+
+        function getSelectedIds() {
+            var selectedIds = [];
+            document.querySelectorAll('#myTable input[type="checkbox"]:checked').forEach(function (checkbox) {
+                var id = checkbox.closest('tr').querySelector('td:nth-child(3)').textContent;
+                selectedIds.push(id);
+            });
+            return selectedIds;
+        }
+
+        // Event listener for the #deleteSelected
+        document.getElementById('deleteSelected').addEventListener('click', function(event) {
+            event.preventDefault();
+
+            var selectedIds = getSelectedIds();
+            // Populate the hidden input field with selected user IDs
+            var selectedIdsInput = document.getElementById('selectedDeleteIds');
+            selectedIdsInput.value = selectedIds.join(',');
+        });
+
+
+        // Event listener for the #deleteSelected
+        document.getElementById('blockSelected').addEventListener('click', function(event) {
+            event.preventDefault();
+
+            var selectedIds = getSelectedIds();
+            // Populate the hidden input field with selected user IDs
+            var selectedIdsInput = document.getElementById('selectedBlockIds');
+            selectedIdsInput.value = selectedIds.join(',');
+        });
+
     });
-
-  </script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</script>
 
     <?php include('../includes/footer.php'); ?>
 
